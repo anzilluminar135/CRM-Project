91ae2d106788f1c518c1b920b125314fbdbe10d7
anzilluminar135 <anzilluminar135@gmail.com>
2025-05-17 16:39:37 +0530

    Update settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -142,6 +142,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = ['static']
 
+STATIC_ROOT = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
